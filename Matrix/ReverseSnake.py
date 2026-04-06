971facca469f27078a995afa137379c024665906
Src-Code-T <netfluxandphysics@gmail.com>
2026-04-06 22:42:47 +0530

    Reverse Snake Column Traversal

--- Matrix/ReverseSnake.py
+++ Matrix/ReverseSnake.py
@@ -19,3 +19,19 @@ print("Reverse Snake Pattern Row Wise:")
 print(snake_row_reverse(matrix))
 
 
+def snake_col_reverse(matrix):
+    res = []
+    rows, cols = len(matrix), len(matrix[0])
+    
+    for j in range(cols):
+        if j % 2 == 0:
+            for i in range(rows - 1, -1, -1):
+                res.append(matrix[i][j])
+        else:
+            for i in range(rows):
+                res.append(matrix[i][j])
+    return res
+
+# Example usage:
+print("\nReverse Snake Pattern Column Wise:")
+print(snake_col_reverse(matrix))
